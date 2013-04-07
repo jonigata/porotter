4cd486f90f6a7cd11874eac732264f3dba4fa15e
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2013-04-07 23:05:25 +0900

    improve security

--- server/app.rb
+++ server/app.rb
@@ -15,7 +15,7 @@ class Porotter < Sinatra::Base
   end
 
   before do
-    ensure_login_user_except(['users'])
+    ensure_login_user_except(['users', 'logout'])
   end
 
   get '/' do
@@ -23,9 +23,15 @@ class Porotter < Sinatra::Base
     redirect local_url("/users/#{@user.store.username}/myboard");
   end
 
+  get '/logout' do
+    session["user_id"] = nil
+    go_to_login_page
+  end
+
   get '/users/*/*' do |username, boardname|
-    target = User.store_class.find_by_username(username) or go_to_login_page
-    board = target.find_board(boardname) or go_to_login_page
-    erb :board, :locals => { :current_board => board, :boards => @user.store.boards }
+    halt 403 if !@user
+    owner = User.store_class.find_by_username(username) or go_to_login_page
+    board = owner.find_board(boardname) or go_to_login_page
+    erb :board, :locals => { :owner => owner, :current_board => board, :boards => owner.store.boards }
   end
 end
